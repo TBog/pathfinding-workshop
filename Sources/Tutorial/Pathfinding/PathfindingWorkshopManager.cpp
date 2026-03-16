bc89d9af7b29f2a33f20bc97c011e706fa4d1a02
TBog <thetbog@gmail.com>
2026-03-16 12:40:14 +0200

    Refactor signed-area exercise; add IsCollinear
    Reorganize _RunSignedAreaExercise to clearly separate the IsLeft exercise and add a new IsCollinear exercise. The IsLeft block now generates circular test points, uses a rotating line, applies per-point height offsets, and renders debug lines/spheres/text/triangles while comparing user vs control IsLeft results. Added a new IsCollinear block with fixed line and test points (left, on, right), performing the same debug rendering and comparison using IsCollinear. Overall cleanup reduces duplicated code and improves visual debugging of the geometry tests.

--- Sources/Tutorial/Pathfinding/PathfindingWorkshopManager.cpp
+++ Sources/Tutorial/Pathfinding/PathfindingWorkshopManager.cpp
@@ -66,42 +66,76 @@ void PathfindingWorkshopManager::Update(float dt)
 
 void PathfindingWorkshopManager::_RunSignedAreaExercise()
 {
-	const int pointsCount = 8;
-	Vector3 points[pointsCount];
-	for (int i = 0; i < pointsCount; i++)
+	// IsLeft exercise with a rotating line and some generated points in a circle around the line
 	{
-		float angle = (float)i / (float)pointsCount * 2.f * (float)D3DX_PI;
-		points[i] = Vector3(cosf(angle), 0.f, sinf(angle)) * 3.f;
+		const int pointsCount = 8;
+		Vector3 points[pointsCount];
+		for (int i = 0; i < pointsCount; i++)
+		{
+			float angle = (float)i / (float)pointsCount * 2.f * (float)D3DX_PI;
+			points[i] = Vector3(cosf(angle), 0.f, sinf(angle)) * 3.f;
+		}
+
+		const Vector3 lineA(cosf(m_RotatingAngle) * 5.f, 0.f, sinf(m_RotatingAngle) * 5.f);
+		const Vector3 lineB(cosf(m_RotatingAngle) * -5.f, 0.f, sinf(m_RotatingAngle) * -5.f);
+
+		for (int i = 0; i < pointsCount; i++)
+		{
+			Vector3 p1 = lineA;
+			Vector3 p2 = lineB;
+			Vector3 p3 = points[i];
+
+			// have all points on the same Y coordinate
+			const float height = i * .5f;
+			p1.y = height;
+			p2.y = height;
+			p3.y = height;
+
+			g_debugRender->AddLine(p1, p2, COLOR_WHITE);
+			g_debugRender->AddSphere(p1, .05f, COLOR_RED);
+			g_debugRender->AddSphere(p2, .05f, COLOR_BLUE);
+
+			WCHAR msg[32];
+			swprintf_s(msg, ARRAYSIZE(msg), L"<%d>", i);
+			g_debugRender->AddText(p3, msg, COLOR_BLACK, WithAlpha(COLOR_YELLOW, .75f));
+
+			Vector2 testPoints[] = { Vector2(p1.x, p1.z), Vector2(p2.x, p2.z), Vector2(p3.x, p3.z) };
+			const bool matchingIsLeft = m_UserWorkSheet->IsLeft(testPoints[0], testPoints[1], testPoints[2]) == m_ControlWorkSheet->IsLeft(testPoints[0], testPoints[1], testPoints[2]);
+
+			// use debug render to draw a red or green check for each test
+			g_debugRender->AddTriangle(p1, p2, p3, matchingIsLeft ? Color(0.f, 1.f, .5f, .25f) : Color(1.f, 0.f, .5f, .25f));
+			g_debugRender->AddSphere(p3, .05f, matchingIsLeft ? COLOR_GREEN : COLOR_MAGENTA);
+		}
 	}
 
-	const Vector3 lineA(cosf(m_RotatingAngle) * 5.f, 0.f, sinf(m_RotatingAngle) * 5.f);
-	const Vector3 lineB(cosf(m_RotatingAngle) * -5.f, 0.f, sinf(m_RotatingAngle) * -5.f);
-
-	for (int i = 0; i < pointsCount; i++)
+	// IsCollinear exercise with some generated points to the left and some to the right of the line, and some on the line
 	{
-		Vector3 p1 = lineA;
-		Vector3 p2 = lineB;
-		Vector3 p3 = points[i];
-		
-		// have all points on the same Y coordinate
-		const float height = i * .5f;
-		p1.y = height;
-		p2.y = height;
-		p3.y = height;
+		const Vector3 lineA(10.f, 0.f, 0.f);
+		const Vector3 lineB(11.f, 0.f, 0.f);
+		const int pointsCount = 6;
+		const Vector3 points[pointsCount] = { {9.f, 0.f, -1.f}, {9.f, 0.f, 0.f}, {9.f, 0.f, 1.f}, {12.f, 0.f, -1.f}, {12.f, 0.f, 0.f}, {12.f, 0.f, 1.f} };
+		for (int i = 0; i < pointsCount; i++)
+		{
+			Vector3 p1 = lineA;
+			Vector3 p2 = lineB;
+			Vector3 p3 = points[i];
+			// have all points on the same Y coordinate
+			const float height = i * .5f;
+			p1.y = height;
+			p2.y = height;
+			p3.y = height;
+			g_debugRender->AddLine(p1, p2, COLOR_WHITE);
+			g_debugRender->AddSphere(p1, .05f, COLOR_RED);
+			g_debugRender->AddSphere(p2, .05f, COLOR_BLUE);
+			WCHAR msg[32];
+			swprintf_s(msg, ARRAYSIZE(msg), L"<%d>", i);
+			g_debugRender->AddText(p3, msg, COLOR_BLACK, WithAlpha(COLOR_YELLOW, .75f));
+			Vector2 testPoints[] = { Vector2(p1.x, p1.z), Vector2(p2.x, p2.z), Vector2(p3.x, p3.z) };
+			const bool matchingIsCollinear = m_UserWorkSheet->IsCollinear(testPoints[0], testPoints[1], testPoints[2]) == m_ControlWorkSheet->IsCollinear(testPoints[0], testPoints[1], testPoints[2]);
 
-		g_debugRender->AddLine(p1, p2, COLOR_WHITE);
-		g_debugRender->AddSphere(p1, .05f, COLOR_RED);
-		g_debugRender->AddSphere(p2, .05f, COLOR_BLUE);
-
-		WCHAR msg[32];
-		swprintf_s(msg, ARRAYSIZE(msg), L"<%d>", i);
-		g_debugRender->AddText(p3, msg, COLOR_BLACK, WithAlpha(COLOR_YELLOW, .75f));
-		
-		Vector2 testPoints[] = { Vector2(p1.x, p1.z), Vector2(p2.x, p2.z), Vector2(p3.x, p3.z) };
-		const bool matchingIsLeft = m_UserWorkSheet->IsLeft(testPoints[0], testPoints[1], testPoints[2]) == m_ControlWorkSheet->IsLeft(testPoints[0], testPoints[1], testPoints[2]);
-
-		// use debug render to draw a red or green check for each test
-		g_debugRender->AddTriangle(p1, p2, p3, matchingIsLeft ? Color(0.f, 1.f, .5f, .25f) : Color(1.f, 0.f, .5f, .25f));
-		g_debugRender->AddSphere(p3, .05f, matchingIsLeft ? COLOR_GREEN : COLOR_MAGENTA);
+			// use debug render to draw a red or green check for each test
+			g_debugRender->AddTriangle(p1, p2, p3, matchingIsCollinear ? Color(0.f, 1.f, .5f, .25f) : Color(1.f, 0.f, .5f, .25f));
+			g_debugRender->AddSphere(p3, .05f, matchingIsCollinear ? COLOR_GREEN : COLOR_MAGENTA);
+		}
 	}
 }
